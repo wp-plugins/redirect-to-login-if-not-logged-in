f19b012455f5795df15350dd3b216a0e41d27892
daankortenbach <daankortenbach@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-31 12:32:49 +0000

    Moved the conditionals to the init hook due to some edge cases not redirecting.
    git-svn-id: https://plugins.svn.wordpress.org/redirect-to-login-if-not-logged-in/trunk@848798 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- dmk-redirect-if-not-logged-in.php
+++ dmk-redirect-if-not-logged-in.php
@@ -1,30 +1,43 @@
 <?php
 /**
  * @package Redirect to login if not logged in
- * @version 1.3
+ * @version 1.4
  */
 /*
 Plugin Name: Redirect to login if not logged in
 Plugin URI: http://wordpress.org/plugins/redirect-to-login-if-not-logged-in/
 Description: Redirect to wp-login.php if user is not logged in.
 Author: Daan Kortenbach
-Version: 1.3
+Version: 1.4
 Author URI: http://daan.kortenba.ch/
 */
 
-add_action( 'wp', 'dmk_not_loggedin_redirect' );
+add_action( 'init', 'dmk_not_loggedin_redirect' );
 
 /**
- * Redirect to wp-login.php if user is not logged in.
+ * Conditional: if user is not logged in and is not on wp-login.php, call redirect on hook 'wp'.
  *
  * @author Daan Kortenbach
  *
  * @global string $pagenow
- * @return void redirect
+ * @return void
  */
 function dmk_not_loggedin_redirect() {
 	global $pagenow;
 
-	if ( ! is_user_logged_in() && $pagenow != 'wp-login.php' )
-		wp_redirect( wp_login_url(), 302 );
+	if ( ! is_user_logged_in() && $pagenow != 'wp-login.php' ) {
+		add_action( 'wp', 'dmk_not_loggedin_do_redirect' );
+	}
+}
+
+/**
+ * Redirect to wp-login.php.
+ *
+ * @author Daan Kortenbach
+ *
+ * @return void redirect
+ */
+function dmk_not_loggedin_do_redirect() {
+	wp_redirect( wp_login_url(), 302 );
+	exit;
 }
--- readme.txt
+++ readme.txt
@@ -3,8 +3,8 @@ Contributors: daankortenbach
 Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=BKUCJXJJ8XJVY
 Tags: redirect, login
 Requires at least: 3.0.1
-Tested up to: 3.6
-Stable tag: 1.3
+Tested up to: 3.8.1
+Stable tag: 1.4
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -20,6 +20,9 @@ Redirects site visitors to the login page if the visitor is not logged in. This 
 1. Activate the plugin through the 'Plugins' menu in WordPress
 
 == Changelog ==
+
+= 1.4 =
+* Moved the conditionals to the init hook due to some edge cases not redirecting.
 
 = 1.3 =
 * Plugin naming.
