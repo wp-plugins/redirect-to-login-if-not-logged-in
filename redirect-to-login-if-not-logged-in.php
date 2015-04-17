59373d7935710940f7c0119b934db58863315e13
daankortenbach <daankortenbach@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-17 17:29:46 +0000

    1.6.2
    git-svn-id: https://plugins.svn.wordpress.org/redirect-to-login-if-not-logged-in/trunk@1137389 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ redirect-to-login-if-not-logged-in.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @package Redirect to login if not logged in
+ * @version 1.6.2
+ */
+/*
+Plugin Name:   Redirect to login if not logged in
+Plugin URI:    http://wordpress.org/plugins/redirect-to-login-if-not-logged-in/
+Description:   Redirects a user to the login page if the user is not logged in. After login the user gets redirected to the original entry page.
+Author:        Daan Kortenbach
+Version:       1.6.2
+Author URI:    http://daan.kortenba.ch/
+License:       GPL-2.0 or later
+License URI:   http://www.gnu.org/licenses/gpl-2.0.txt
+*/
+
+add_action( 'parse_request', 'dmk_redirect_to_login_if_not_logged_in', 1 );
+/**
+ * Redirects a user to the login page if not logged in.
+ *
+ * @author Daan Kortenbach
+ */
+function dmk_redirect_to_login_if_not_logged_in() {
+	is_user_logged_in() || auth_redirect();
+}
+
+
+add_filter( 'login_url', 'dmk_strip_loggedout', 1, 1 );
+/**
+ * Strips '?loggedout=true' from redirect url after login.
+ *
+ * @author Daan Kortenbach
+ *
+ * @param  string $login_url
+ * @return string $login_url
+ */
+function dmk_strip_loggedout( $login_url ) {
+	return str_replace( '%3Floggedout%3Dtrue', '', $login_url );
+}
